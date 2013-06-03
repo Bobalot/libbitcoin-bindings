b9ad28c9955793c58c437b04f0dd07d3364f93b0
Pablo Martin <caedes@riseup.net>
2013-06-03 21:19:37 +0200

    report using an internal function.

--- tests/fullnode.py
+++ tests/fullnode.py
@@ -29,7 +29,10 @@ class fullnode(object):
 				     self._poller,
 				     self._txpool)
         self._session = session(self._net_pool, pars)
-        print "[fullnode] ok"
+        self.report("[fullnode] ok")
+
+    def report(self, text, *args):
+        print text + " ".join(map(lambda s: str(s), args))
 
     def start(self):
         self._protocol.subscribe_channel(self.monitor_tx)
@@ -37,7 +40,7 @@ class fullnode(object):
         self._chain.subscribe_reorganize(self.on_reorganize)
         self._txpool.start()
         self._session.start(self.on_session_start)
-        print "[fullnode.start] ok"
+        self.report("[fullnode.start] ok")
 
     def stop(self):
         self._session.stop(self.on_session_stop)
@@ -48,58 +51,58 @@ class fullnode(object):
         self._disk_pool.join()
         self._mem_pool.join()
         self._chain.stop()
-        print "[fullnode.stop] ok"
+        self.report("[fullnode.stop] ok")
 
     def on_chain_start(self, ec):
-        print "[fullnode.chain] started", ec
+        self.report("[fullnode.chain] started ", ec)
 
     def on_session_stop(self, ec):
-        print "[fullnode.session] stopped", ec
+        self.report("[fullnode.session] stopped", ec)
 
     def on_session_start(self, ec):
-        print "[fullnode.session] started", ec
+        self.report("[fullnode.session] started", ec)
         if ec:
             self.stop()
             sys.exit(1)
 
     def on_reorganize(self, ec, height, arrivals, replaced):
-        print '[fullnode.reorganize]', height, str(ec), len(arrivals), len(replaced)
+        self.report('[fullnode.reorganize]', height, str(ec), len(arrivals), len(replaced))
         if len(arrivals):
-            print ' arrival', print_block(arrivals[0])
+            self.report(' arrival', print_block(arrivals[0]))
         if len(list2):
-            print ' replaced', print_block(arrivals[1])
+            self.report(' replaced', print_block(arrivals[1]))
         self._chain.subscribe_reorganize(self.on_reorganize)
 
     def monitor_tx(self, node):
-        print "(fullnode.tx)", node
+        self.report("(fullnode.tx)", node)
         node.subscribe_transaction(lambda ec, tx: self.recv_tx(node, tx, ec))
         self._protocol.subscribe_channel(self.monitor_tx)
 
     def handle_confirm(self, ec):
-        print "(fullnode.store) confirm", ec
+        self.report("(fullnode.store) confirm", ec)
 
     def recv_tx(self, node, tx, ec):
-        print "(fullnode.recv_tx)", ec, tx
+        self.report("(fullnode.recv_tx)", ec, tx)
         if ec:
-            print "error", ec
+            self.report("error", ec)
             return
-        print ' *', len(tx.inputs), len(tx.outputs)
+        self.report(' *', len(tx.inputs), len(tx.outputs))
         self._txpool.store(tx, self.handle_confirm, lambda _ec, u: self.new_unconfirm_valid_tx(node, tx, u, _ec))
         node.subscribe_transaction(lambda _ec, _tx: self.recv_tx(node, _tx, _ec))
 
     def new_unconfirm_valid_tx(self, node, tx, unconfirmed, ec):
-        print "(fullnode.valid_tx)", ec, tx, unconfirmed
+        self.report("(fullnode.valid_tx)", ec, tx, unconfirmed)
         tx_hash = hash_transaction(tx)
         if ec:
-            print "Error", ec
+            self.report("Error", ec)
         else:
-            print "Accepted transaction"
-            print unconfirmed.__class__
+            self.report("Accepted transaction")
+            self.report(unconfirmed.__class__)
             if not unconfirmed.empty():
-               print "Unconfirmed"
+               self.report("Unconfirmed")
                for idx in unconfirmed: 
-                   print ' ', idx
-            print tx_hash
+                   self.report(' ', idx)
+            self.report(tx_hash)
 
 
 if __name__ == '__main__':
