7dbd880ff91dd882bfc52303f337a0c345029b01
Amir Taaki <genjix@riseup.net>
2013-06-16 12:44:41 +0200

    give the balance of a Bitcoin address.

---
+++ tests/balance.py
@@ -0,0 +1,43 @@
+import bitcoin
+import sys
+import time
+
+def blockchain_started(ec, chain, address):
+    if ec:
+        print >> sys.stderr, str(ec)
+    print "Blockchain started"
+    addr = bitcoin.payment_address(address)
+    bitcoin.fetch_history(chain, addr,
+        lambda ec, outs, ins: fetched(ec, outs, ins, chain))
+
+def fetched(ec, outpoints, inpoints, chain):
+    if ec:
+        print >> sys.stderr, str(ec)
+    unspent = []
+    for outpoint, inpoint in zip(outpoints, inpoints):
+        # If the hash is all zeroes, then this output is unspent.
+        if inpoint.hash == "\0" * 32:
+            unspent.append(outpoint)
+    bitcoin.fetch_output_values(chain, unspent, fetched_values)
+
+def fetched_values(ec, values):
+    if ec:
+        print >> sys.stderr, str(ec)
+    print sum(values)
+
+def main(address):
+    pool = bitcoin.threadpool(2)
+    chain = bitcoin.leveldb_blockchain(pool)
+    chain.start("database", lambda ec: blockchain_started(ec, chain, address))
+    raw_input()
+    pool.stop()
+    pool.join()
+    chain.stop()
+
+if __name__ == "__main__":
+    #main("1DC4sirYJ9SuYqPkMQFosJ63ue46knSw2o")
+    if len(sys.argv) != 2:
+        print "Usage: balance ADDRESS"
+    else:
+        main(sys.argv[1])
+
